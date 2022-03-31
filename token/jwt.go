2c61e37821739992fd06a8b4dd54ce366d261322
asrofil <riidloa@gmail.com>
2022-03-31 17:37:33 +0700

    add feature to parse id and validate token

--- token/jwt.go
+++ token/jwt.go
@@ -2,12 +2,17 @@ package token
 
 import (
 	"anya-day/utils"
+	"fmt"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt"
 )
 
+// generate jwt token with HS256 alg
 func GenerateToken(uid int) (string, error) {
 	lifespan := utils.StringToIntIgnore((utils.GetEnvWithFallback("TOKEN_HOUR_LIFESPAN", "1")))
 
@@ -19,4 +24,44 @@ func GenerateToken(uid int) (string, error) {
 	})
 
 	return token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+}
+
+// extract token from query or authorization header
+func ExtractToken(c *gin.Context) string {
+	if token := c.Query("token"); token != "" {
+		return token
+	}
+
+	bearerLine := strings.Split(c.Request.Header.Get("Authorization"), " ")
+
+	if len(bearerLine) == 2 {
+		return bearerLine[1]
+	}
+
+	return ""
+}
+
+// extract user_id from payload of given of jwt token at header or query
+func ExtractUID(c *gin.Context) (uint, error) {
+	t := ExtractToken(c)
+	token, err := jwt.Parse(t, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %s", t.Header["alg"])
+		}
+		return []byte(os.Getenv("JWT_SECRET")), nil
+	})
+
+	if err != nil {
+		return 0, err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		uid, err := strconv.ParseUint(fmt.Sprintf("%.0f", claims["user_id"].(float64)), 10, 32)
+		if err != nil {
+			return 0, err
+		}
+		return uint(uid), nil
+	}
+	return 0, nil
 }
