522f488ed4dce980f546ba33f89816a8bbd7c678
asrofil <riidloa@gmail.com>
2022-03-31 11:34:03 +0700

    update with router and swaggo

--- main.go
+++ main.go
@@ -1,13 +1,36 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"anya-day/config"
+	"anya-day/routes"
+	"anya-day/utils"
+
+	"anya-day/docs"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	r := gin.Default()
+	if env := utils.Getenv("ENVIRONMENT", "development"); env == "development" {
+		err := godotenv.Load()
+		if err != nil {
+			panic(err.Error())
+		}
+	}
 
-	r.GET("/", func(c *gin.Context) {
-		c.Data(200, "text/plain", []byte("Bismillah Final Project"))
-	})
+	db := config.ConnectDataBase()
+
+	sqlDB, _ := db.DB()
+	defer sqlDB.Close()
+
+	// programmatically set swagger info
+	docs.SwaggerInfo.Title = "Anya Day API"
+	docs.SwaggerInfo.Description = "API provide backend service for your ecommerce app"
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.Host = utils.Getenv("SWAGGER_HOST", "localhost:8080")
+	docs.SwaggerInfo.Schemes = []string{"https", "http"}
+
+	r := routes.InitRoute(db)
 
 	r.Run()
 }
