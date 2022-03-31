44fa244f2a7737046d59496504a4a19605e2f879
asrofil <riidloa@gmail.com>
2022-03-31 11:33:20 +0700

    add db config

---
+++ config/db.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"anya-day/models"
+	"fmt"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectDataBase() *gorm.DB {
+	var username, password, host, port, dbname, sslmode string
+
+	username = os.Getenv("DATABASE_USERNAME")
+	password = os.Getenv("DATABASE_PASSWORD")
+	host = os.Getenv("DATABASE_HOST")
+	port = os.Getenv("DATABASE_PORT")
+	dbname = os.Getenv("DATABASE_NAME")
+	sslmode = os.Getenv("SSL_MODE")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s", host, username, password, dbname, port, sslmode)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	db.AutoMigrate(&models.User{}, &models.UserCredential{})
+
+	return db
+
+}
