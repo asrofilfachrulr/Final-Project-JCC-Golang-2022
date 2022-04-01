6dcb30eb98b5412bbd71a0bbf08be4e4e71323ce
asrofil <riidloa@gmail.com>
2022-04-01 23:04:01 +0700

    add dummies into unified file

---
+++ models/sql/dummies.go
@@ -0,0 +1,12 @@
+package models
+
+var UsersDummy = []User{
+	{FullName: "John Doe", Username: "john", Email: "john@mail.com"},
+	{FullName: "Mary Sue", Username: "mary", Email: "mary@mail.com"},
+	{FullName: "Xi Ng", Username: "xi", Email: "nihaoma@mail.com"},
+	{FullName: "Mark Bob", Username: "mark", Email: "mark@mail.com"},
+	{FullName: "Patricia Ng", Username: "ng", Email: "ng@mail.com"},
+	{FullName: "Jack Tyler", Username: "jack", Email: "jack@mail.com"},
+	{FullName: "Tony Like", Username: "tony", Email: "tony@mail.com"},
+	{FullName: "Andy Lim", Username: "andy", Email: "andy@mail.com"},
+}
