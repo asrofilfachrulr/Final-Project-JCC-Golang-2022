dac0bca86e0ea82c5c37d89c7b6f7b8888f36358
asrofil <riidloa@gmail.com>
2022-04-02 00:24:20 +0700

    models for post address

--- models/web/user.go
+++ models/web/user.go
@@ -10,4 +10,19 @@ type (
 		Username string `json:"username"`
 		Email    string `json:"email"`
 	}
+	AddressInput struct {
+		AddressLine string `json:"address_line" binding:"required"`
+		City        string `json:"city" binding:"required"`
+		Country     string `json:"country" binding:"required"`
+		PhoneNumber uint   `json:"phone_number" binding:"required"`
+		PostalCode  uint   `json:"postal_code"`
+	}
+	AddressRespData struct {
+		UserID      uint   `json:"user_id"`
+		AddressLine string `json:"address"`
+		City        string `json:"city"`
+		Country     string `json:"country"`
+		PhoneNumber uint   `json:"number"`
+		PostalCode  uint   `json:"postal_code,omitempty"`
+	}
 )
