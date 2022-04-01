ee0e61a8baabb1d05c53786cecb9079c140c35dd
asrofil <riidloa@gmail.com>
2022-04-01 20:23:52 +0700

    use flag to determine debugging mode and db build related mode instead load everything in .env

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"anya-day/config"
 	"anya-day/routes"
 	"anya-day/utils"
+	"flag"
 	"os"
 
 	"anya-day/docs"
@@ -12,12 +13,22 @@ import (
 )
 
 func main() {
-	if env := utils.GetEnvWithFallback("ENVIRONMENT", "development"); env == "development" {
+	env := utils.GetEnvWithFallback("ENVIRONMENT", "development")
+	if env == "development" {
 		err := godotenv.Load()
 		if err != nil {
 			panic(err.Error())
 		}
 		docs.SwaggerInfo.Schemes = []string{"http"}
+
+		dropMode := flag.String("drop", "nope", "drop all tables and seeding the db if set to DROP, for debugging purpose")
+		debugMode := flag.String("d", "nope", "logging every sql operation if set to DEBUG")
+		initDBMode := flag.String("initdb", "nope", "seeding db whether with dummies only (USERS) or dev users too (USERS_DEV)")
+		flag.Parse()
+
+		os.Setenv("DROP_TABLES", *dropMode)
+		os.Setenv("DEBUG_MODE", *debugMode)
+		os.Setenv("INIT_DB", *initDBMode)
 	} else {
 		docs.SwaggerInfo.Schemes = []string{"https"}
 	}
@@ -26,9 +37,6 @@ func main() {
 
 	sqlDB, _ := db.DB()
 	defer sqlDB.Close()
-
-	dummiesMode := os.Getenv("INIT_DB")
-	config.InitDB(db, dummiesMode)
 
 	// programmatically set swagger info
 	docs.SwaggerInfo.Title = "Anya Day API"
