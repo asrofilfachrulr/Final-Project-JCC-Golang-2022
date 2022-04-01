5987b8d05ed9b5fd071a709b8a6634fd19473367
asrofil <riidloa@gmail.com>
2022-04-02 00:23:52 +0700

    add post feature

--- models/sql/userAddress.go
+++ models/sql/userAddress.go
@@ -1,17 +1,38 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	wmodels "anya-day/models/web"
+
+	"gorm.io/gorm"
+)
 
 type (
 	UserAddress struct {
 		gorm.Model
-		User        User    `gorm:"constraint:OnDelete:CASCADE;" json:"-"`
-		Country     Country `gorm:"constraint:OnDelete:CASCADE;" json:"-"`
-		UserID      uint    `gorm:"not null"`
-		AddressLine string  `gorm:"not null"`
-		City        string  `gorm:"not null"`
-		CountryID   uint    `gorm:"not null"`
-		PhoneNumber uint    `gorm:"not null"`
+		User        User `gorm:"constraint:OnDelete:CASCADE;"`
+		Country     Country
+		UserID      uint   `gorm:"not null"`
+		AddressLine string `gorm:"not null"`
+		City        string `gorm:"not null"`
+		CountryID   uint   `gorm:"not null"`
+		PhoneNumber uint   `gorm:"not null"`
 		PostalCode  uint
 	}
 )
+
+func (ua *UserAddress) PostAddress(db *gorm.DB, data *wmodels.AddressInput) error {
+	country := &Country{}
+	db.Where(&Country{Name: data.Country}).Find(country)
+
+	ua.CountryID = country.ID
+	ua.AddressLine = data.AddressLine
+	ua.City = data.City
+	ua.PhoneNumber = data.PhoneNumber
+	ua.PostalCode = data.PostalCode
+
+	if err := db.Save(ua).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
