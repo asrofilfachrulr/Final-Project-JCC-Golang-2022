e28be108f857c0daea69af33aa8a568444829546
asrofil <riidloa@gmail.com>
2022-04-02 07:18:31 +0700

    add validate digit helper

--- utils/helper.go
+++ utils/helper.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
@@ -19,6 +20,22 @@ func StringToIntIgnore(s string) int {
 	n, _ := strconv.Atoi(s)
 
 	return n
+}
+
+func CountDigits(n int) int {
+	count := 1
+	for r := n / 10; r != 0; r /= 10 {
+		count += 1
+	}
+	return count
+}
+
+func ValidateDigitInt(n int, min int, max int, desc string) error {
+	digits := CountDigits(n)
+	if digits < min || digits > max {
+		return fmt.Errorf("%s is not valid, digit is too long or too short", desc)
+	}
+	return nil
 }
 
 func ParseFromJSON(req io.ReadCloser, data interface{}) error {
